8cde8a3f9fc160b423b0e847fb952730ba90b43c
ssgharat <57014896+ssgharat@users.noreply.github.com>
2020-08-02 18:40:55 -0700

    Update lawnmover_shubham.py
    Updated description

--- lawnmover_shubham.py
+++ lawnmover_shubham.py
@@ -1,6 +1,6 @@
-# this program moves the turtlesim in lawnmover pattern. The distance is scaled due to turtlesim node contraints.
-# we can create a new turtle at the bottom left of the turtlesim node by using rosservice call /spawn 1.0 1.0 0 t2
-# and then scale the distance accordingly
+#This program moves the turtlebot in lawnmover pattern using the turtlesim node on ROS. The distance of motion is scaled due to turtlesim node contraints,
+#we can create a new turtle at the bottom left of the turtlesim node by using rosservice call /spawn 1.0 1.0 0 t2  or at any spot required by changing the coordinates. 
+#The command rosservice call/ kill (name_of_turtle) to make the turtle disappear.
 
 # importing necessary packages
 import rospy
